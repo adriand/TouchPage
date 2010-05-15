c42f4c51395ae9cb74ccd9e7026ec0424bbb94ee
Adrian Duyzer <adrian@Macbook-Pro.local>
2010-05-15 07:57:00 -0400

    working image uploading and image creation

--- classes.rb
+++ classes.rb
@@ -1,15 +1,22 @@
 class Item
   include DataMapper::Resource
+  include Paperclip::Resource
   
   property :id, Serial
+  property :title, String
   property :uri, String
   property :width, Integer
   property :height, Integer
   
-  # storing the created_at timestamp will help us track site participation
-  # in order to have this set for us automatically, we've added:
-  # require 'dm-timestamps'
-  # to the list of required gems.
+  property :image_file_name, String
+  property :image_content_type, String
+  property :image_file_size, Integer
+  property :image_updated_at, DateTime
+  
   property :created_at, DateTime
   
+  has_attached_file :image, 
+                    :styles => { :thumb => "200x200#" },
+                    :url => "/system/:attachment/:id/:style/:basename.:extension",
+                    :path => "#{Dir.pwd}/public/system/:attachment/:id/:style/:basename.:extension" 
 end
--- respite.rb
+++ respite.rb
@@ -5,6 +5,7 @@ require 'dm-core'
 require 'dm-timestamps'
 require 'haml'
 require 'sass'
+require 'dm-paperclip'
 require 'ruby-debug'
 
 configure :development do
@@ -24,7 +25,34 @@ DataMapper.auto_upgrade!
 #DataMapper.auto_migrate!
 
 get '/' do
+  @items = Item.all
   haml :index
+end
+
+get '/new' do
+  haml :new
+end
+
+post '/create' do
+  @item = Item.new(params[:item])
+  @item.image = make_paperclip_mash(params[:image])
+  if @item.save
+    redirect '/new'
+  else
+    haml :new
+  end
+end
+
+get '/manage' do
+  @items = Item.all
+  haml :manage
+end
+
+# TODO: shouldn't be a get
+get '/destroy/:id' do
+  @item = Item.get(params[:id])
+  @item.destroy
+  redirect '/manage'
 end
 
 get '/style.css' do
@@ -34,4 +62,14 @@ end
 
 helpers do  
   
+end
+
+# http://blog.wyeworks.com/2010/2/10/making-paperclip-work-with-sinatra-datamapper
+def make_paperclip_mash(file_hash)
+  mash = Mash.new
+  mash['tempfile'] = file_hash[:tempfile]
+  mash['filename'] = file_hash[:filename]
+  mash['content_type'] = file_hash[:type]
+  mash['size'] = file_hash[:tempfile].size
+  mash
 end
