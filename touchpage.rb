883676ffcb8762a854fec9df1af940d9bf221b38
Adrian Duyzer <adrian@Macbook-Pro.local>
2010-05-17 22:00:07 -0400

    autoupgrade

--- touchpage.rb
+++ touchpage.rb
@@ -17,8 +17,8 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://touchpage.db')
 
 require 'classes'
 
-#DataMapper.auto_upgrade!
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
+#DataMapper.auto_migrate!
 
 get '/' do
   @items = Item.all
